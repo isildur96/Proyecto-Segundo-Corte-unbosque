85e7b362a81f8fa30a5f3738852d87f9543aaf4e
juane <juane@DESKTOP-68G77BI>
2023-04-16 21:59:07 -0500

    version 0.2 amigo parcialmente creado

---
+++ AGENDA DIGITAL ROSITA PLUS/src/co/edu/unbosque/model/Amigo.java
@@ -0,0 +1,63 @@
+package co.edu.unbosque.model;
+
+public class Amigo {
+
+	private String nombre;
+	private String pais;
+	private String telefono;
+	private String email;
+	
+	
+	public Amigo(String nombre, String pais, String telefono, String email) {
+		super();
+		this.nombre = nombre;
+		this.pais = pais;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getPais() {
+		return pais;
+	}
+
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+}
